d460cc05f0d39c574d0c8d4c2b836a151c721f6e
Michal <mwargan@gmail.com>
2025-01-21 13:09:51 +0200

    add referral component to homescreen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { useUserStore } from "../stores/user";
-
-const userStore = useUserStore();
-
-const referral = ref(userStore.user?.referral_code);
-
-const copyUrl = () => {
-  const url = `https://app.guidescout.com/sign-up-as-guide?referral=${referral.value}`;
-  navigator.clipboard.writeText(url);
-};
+import ReferralLink from "@/components/ReferralLink.vue";
 </script>
 
 <template>
@@ -18,12 +8,6 @@ const copyUrl = () => {
     <b>{{ $t("Your profile is still being verified.") }}</b>
     <p>{{ $t("Why not refer a friend to earn some money?") }}</p>
 
-    <input
-      type="text"
-      :value="`https://app.guidescout.com/sign-up-as-guide?referral=${referral}`"
-      readonly
-    />
-
-    <router-link to="/referrals">{{ $t("Refer a friend") }}</router-link>
+    <referral-link />
   </div>
 </template>
